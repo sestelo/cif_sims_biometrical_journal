2e373e3318da2156f7a82e64fe51972ab3a04cb2
Marta Sestelo <sestelo@uvigo.es>
2023-09-18 14:22:35 +0200

    Update app.R

--- 05_app_data/app.R
+++ 05_app_data/app.R
@@ -19,13 +19,13 @@ table(ebmt2$cod) # causes of death
 out <- kcifcurves(time = ebmt2$time, status = ebmt2$status, x = NULL,
                   k = 1, kbin = 50, algorithm = "kmeans")
 
-# Plotting the CIFs without grouping
+# Plotting the CIFs without grouping (Figure 5)
 autoplot(out, groups_by_colour = FALSE) + 
   ggplot2::theme(legend.position="none")
 
 
 
-# Testing the number of groups
+# Testing the number of groups (Figure 6)
 outtest <- survcifcurves(time = ebmt2$time, status = ebmt2$status, x = NULL,
                          kvector = NULL,  kbin = 50, nboot = 200,
                          algorithm = "kmeans", cluster = TRUE)
